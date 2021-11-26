c745e6556d2655d4020210a5b92424a5b2960692
Mythreya <mythreya.github@gmail.com>
2021-11-26 13:43:22 -0800

    Update CORS, gitignore

--- resdb/__init__.py
+++ resdb/__init__.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__,
     static_url_path='/',
     static_folder='../static')
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config.from_file('../config.json', json.load)
 
